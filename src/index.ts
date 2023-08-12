d5b11e4f1df005df2b14f2197148b0fbfec1976b
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-08-13 01:40:30 +0800

    chore: support @property

--- src/index.ts
+++ src/index.ts
@@ -113,6 +113,33 @@ export function transfer(md: string) {
           })
         }
       }
+      else if (token.type === 'list') {
+        token.items.forEach((item: any) => {
+          const text = item.text.replace(/\s+/g, ' ').split(' ')
+          const prop = text[0]
+          if (prop[0] === '@') {
+            const propName = prop.slice(1)
+            if (propName === 'property') {
+              props[text[2]] = {
+                value: '',
+                description: text[3],
+                default: '',
+                type: text[1].slice(1, -1),
+              }
+            }
+            else if (propName === 'event') {
+              events.push({
+                name: text[2],
+                description: text[3],
+                params: text[1].slice(1, -1),
+              })
+            }
+          }
+          else {
+            name = prop
+          }
+        })
+      }
     })
 
     return JSON.stringify({
@@ -128,9 +155,22 @@ export function transfer(md: string) {
   }
 }
 
-transfer(`| 名称                           | 描述                 | 版本 |    
-| ------------------------------ | ------------------------------------------------- | ---- |
-| goTo(slideNumber, dontAnimate) | 切换到指定面板, dontAnimate = true 时，不使用动画 |      |
-| next()                         | 切换到下一面板                                    |      |
-| prev()                         | 切换到上一面板                                    |      |
+transfer(`
+/**
+ * alertTips 警告提示
+ * @description 警告提示，展现需要关注的信息
+ * @tutorial https://uviewui.com/components/alertTips.html
+ * @property {String} title 显示的标题文字
+ * @property {String} description 辅助性文字，颜色比title浅一点，字号也小一点，可选
+ * @property {String} type 关闭按钮(默认为叉号icon图标)
+ * @property {String} icon 图标名称
+ * @property {Object} icon-style 图标的样式，对象形式
+ * @property {Object} title-style 标题的样式，对象形式
+ * @property {Object} desc-style 描述的样式，对象形式
+ * @property {String} close-able 用文字替代关闭图标，close-able为true时有效
+ * @property {Boolean} show-icon 是否显示左边的辅助图标
+ * @property {Boolean} show 显示或隐藏组件
+ * @event {Function} click 点击组件时触发
+ * @event {Function} close 点击关闭按钮时触发
+ */
 `)
